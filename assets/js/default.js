3ad27bb3e95201e61475c1df58dfc75289843518
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2021-07-27 11:03:00 -0500

    fix js file

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,15 +1,6 @@
-const menuToggle = document.getElementById('menu-toggle')
-const menu = document.getElementById('menu')
-const callButtons = [...document.querySelectorAll('.site-call')]
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
 
-menuToggle.addEventListener('click', function() {
-  menu.classList.toggle('hidden')
-})
-
-callButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    Calendly.initPopupWidget({
-      url: 'https://calendly.com/lasobremesa/30min'
-    })
-  })
-})
+menuToggle.addEventListener("click", function () {
+  menu.classList.toggle("hidden");
+});
