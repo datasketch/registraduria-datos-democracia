0e2b32fcdbf6cf2925b573b34056e76ca26e88ab
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-09-27 08:55:09 -0500

    finish novedades section

---
+++ assets/js/novedades.js
@@ -0,0 +1,20 @@
+import { setupSlider } from './lib/slider';
+
+setupSlider('.slider-wrapper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: false,
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 58,
+    },
+  },
+});
